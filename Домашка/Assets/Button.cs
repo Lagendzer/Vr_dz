237420a950d773f16bdefb501a2eb0ea3763f71b
Lagendzer <144949752+Lagendzer@users.noreply.github.com>
2023-10-03 00:58:02 +0300

    Vr_dz

---
+++ Домашка/Assets/Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject Sphere;
+    public GameObject Cube;
+    
+    public void onClick()
+    {
+        if (Sphere.activeSelf)
+        {
+            //Прячем фигуры
+            Sphere.SetActive(false);
+
+            Cube.SetActive(false);
+        }
+        else
+        {
+            //Показываем фигуры
+            Sphere.SetActive(true);
+
+            Cube.SetActive(true);
+        }
+    }
+}
